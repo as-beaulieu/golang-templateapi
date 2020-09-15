56fff57627042131ca0401b26086dbc820151c7b
Andrew Beaulieu <as.beaulieu@gmail.com>
2020-09-14 22:07:02 -0600

    Wired up DI for postgres DAO, and Interface for basic example functions

--- src/dao/dao.go
+++ src/dao/dao.go
@@ -1,7 +1,55 @@
 package dao
 
-type DAO interface {
+import "TemplateApi/src/models"
 
+type DAO interface {
+	CreateUser(user models.User) error
+	GetUsers() ([]*models.User, error)
+	DeleteUser(id string) error
 }
 
+type dao struct {
+	host     string
+	port     int
+	user     string
+	password string
+	dbname   string
+}
 
+type PostgresBuilder struct {
+	dao
+}
+
+func (pb PostgresBuilder) SetHost(host string) PostgresBuilder {
+	a := pb
+	a.host = host
+	return a
+}
+
+func (pb PostgresBuilder) SetPort(port int) PostgresBuilder {
+	a := pb
+	a.port = port
+	return a
+}
+
+func (pb PostgresBuilder) SetUser(user string) PostgresBuilder {
+	a := pb
+	a.user = user
+	return a
+}
+
+func (pb PostgresBuilder) SetPassword(pass string) PostgresBuilder {
+	a := pb
+	a.password = pass
+	return a
+}
+
+func (pb PostgresBuilder) SetDbName(name string) PostgresBuilder {
+	a := pb
+	a.dbname = name
+	return a
+}
+
+func (pb PostgresBuilder) Build() *dao {
+	return &pb.dao
+}
---
+++ src/dao/user.go
@@ -0,0 +1,18 @@
+package dao
+
+import "TemplateApi/src/models"
+
+func (d dao) CreateUser(user models.User) error {
+
+	return nil
+}
+
+func (d dao) GetUsers() ([]*models.User, error) {
+
+	return nil, nil
+}
+
+func (d dao) DeleteUser(id string) error {
+
+	return nil
+}
