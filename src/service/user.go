71c0bbd35a1cf69a16fe6fdcaa627f4428c1f257
Andrew Beaulieu <as.beaulieu@gmail.com>
2020-09-27 23:04:38 -0600

    finished user service methods

--- src/service/user.go
+++ src/service/user.go
@@ -3,26 +3,46 @@ package service
 import "TemplateApi/src/models"
 
 func (s service) CreateUser(user models.User) (*models.User, error) {
+	err := s.postgres.CreateUser(user)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	return &user, nil
 }
 
 func (s service) GetUsers() ([]*models.User, error) {
+	users, err := s.postgres.GetUsers()
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	return users, nil
 }
 
 func (s service) GetUserByID(userID string) (*models.User, error) {
+	user, err := s.postgres.GetUserById(userID)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	return user, nil
 }
 
 func (s service) UpdateUser(user models.User) (*models.User, error) {
+	err := s.postgres.UpdateUser(user)
+	if err != nil {
+		return nil, err
+	}
 
 	return nil, nil
 }
 
 func (s service) DeleteUser(userID string) error {
+	err := s.postgres.DeleteUser(userID)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
