656779c185a201e76c0f58c2898bf3e9671c6369
Andrew Beaulieu <as.beaulieu@gmail.com>
2020-09-27 22:39:52 -0600

    finished user service methods

--- src/server/handler_user.go
+++ src/server/handler_user.go
@@ -1,27 +1,43 @@
 package server
 
 import (
+	"TemplateApi/src/models"
 	"TemplateApi/src/service"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func createUserHandler(svc service.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//var exampleModel models.ExampleModel
+		var user models.User
 
-		//decoder := json.NewDecoder(r.Body)
-		//if err := decoder.Decode(&exampleModel); err != nil {
-		//	respondWithJSON(w, r, http.StatusBadRequest, r.Body)
-		//	return
-		//}
-		//defer r.Body.Close()
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&user); err != nil {
+			respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+			return
+		}
+		defer r.Body.Close()
+
+		result, err := svc.CreateUser(user)
+		if err != nil {
+			respondWithJSON(w, r, http.StatusInternalServerError, r.Body)
+			return
+		}
+
+		respondWithJSON(w, r, http.StatusCreated, result)
 	}
 }
 
 func getAllUsersHandler(svc service.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		result, err := svc.GetUsers()
+		if err != nil {
+			respondWithJSON(w, r, http.StatusInternalServerError, r.Body)
+			return
+		}
 
+		respondWithJSON(w, r, http.StatusOK, result)
 	}
 }
 func getUserByIDHandler(svc service.Service) http.HandlerFunc {
@@ -39,14 +55,22 @@ func getUserByIDHandler(svc service.Service) http.HandlerFunc {
 }
 func updateUserHandler(svc service.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//var exampleModel models.ExampleModel
+		var user models.User
 
-		//decoder := json.NewDecoder(r.Body)
-		//if err := decoder.Decode(&exampleModel); err != nil {
-		//	respondWithJSON(w, r, http.StatusBadRequest, r.Body)
-		//	return
-		//}
-		//defer r.Body.Close()
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&user); err != nil {
+			respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+			return
+		}
+		defer r.Body.Close()
+
+		result, err := svc.UpdateUser(user)
+		if err != nil {
+			respondWithJSON(w, r, http.StatusInternalServerError, r.Body)
+			return
+		}
+
+		respondWithJSON(w, r, http.StatusAccepted, result)
 	}
 }
 func deleteUserByIDHandler(svc service.Service) http.HandlerFunc {
@@ -59,6 +83,6 @@ func deleteUserByIDHandler(svc service.Service) http.HandlerFunc {
 			respondWithJSON(w, r, 500, nil)
 		}
 
-		respondWithJSON(w, r, http.StatusAccepted, nil)
+		respondWithJSON(w, r, http.StatusNoContent, nil)
 	}
 }
